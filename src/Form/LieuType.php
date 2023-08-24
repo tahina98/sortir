5d2a14e40d0e19056a39b1c7b6d4b079edcae5b8
cmathieu2023 <catherine.k.mathieu@gmail.com>
2023-08-24 14:36:17 +0200

    NouveauLieu twig recuperation de l'url pour api ok

--- src/Form/LieuType.php
+++ src/Form/LieuType.php
@@ -7,6 +7,8 @@ use App\Entity\Ville;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -20,12 +22,9 @@ class LieuType extends AbstractType
             ->add('latitude')
             ->add('longitude')
             ->add('ville', EntityType::class, [
-                    'class' => Ville::class,
-                    'choice_label' => 'nom'
-                ]
-            )
-            ->add('Enregistrer', SubmitType::class)
-        ;
+                'class' => Ville::class,
+                'choice_label' => 'nom'])
+            ->add('Enregistrer', SubmitType::class);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
