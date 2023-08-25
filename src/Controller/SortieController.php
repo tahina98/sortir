63b4db7ab32a96c239ef0ec906cadfb2a780bd4c
tahina98 <tahina.foucher2023@campus-eni.fr>
2023-08-25 11:42:29 +0200

    Resolution conflit oublie d'un "}"

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -22,24 +22,24 @@ class SortieController extends AbstractController
 {
     #[Route('/', name: '_liste')]
     //TODO Intégrer les rôles
-    //TODO  l'affichage par site / isncrit ou non / organisateur ou non /sorties passées
+        //TODO  l'affichage par site / isncrit ou non / organisateur ou non /sorties passées
         // TODO filtrer par date / nom de la sortie contient
     public function liste(SortieRepository $sortieRepository, Request $requete, ParticipantRepository $participantRepository): Response
     {
         //essaie n2
-        $filtre =new Filtre();
+        $filtre = new Filtre();
         $filtreForm = $this->createForm(FiltreType::class, $filtre);
         $filtreForm->handleRequest($requete);
-        $utilisateur = $participantRepository->findOneBy(["pseudo"=>$this->getUser()->getUserIdentifier()]);
-        $sorties=$sortieRepository->findFiltre($filtre,$utilisateur);
-        return $this->render('sortie/liste.html.twig', compact('sorties','filtreForm'));
+        $utilisateur = $participantRepository->findOneBy(["pseudo" => $this->getUser()->getUserIdentifier()]);
+        $sorties = $sortieRepository->findFiltre($filtre, $utilisateur);
+        return $this->render('sortie/liste.html.twig', compact('sorties', 'filtreForm'));
 
     }
 
     #[Route('/creation', name: '_creation')]
-    public function creation (
+    public function creation(
         EntityManagerInterface $entityManager,
-        Request $requete
+        Request                $requete
     ): Response
     {
         $sortie = new Sortie();
@@ -47,7 +47,7 @@ class SortieController extends AbstractController
         $sortieForm = $this->createForm(SortieType::class, $sortie);
         $sortieForm->handleRequest($requete);
 
-        if ($sortieForm->isSubmitted() && $sortieForm->isValid()){
+        if ($sortieForm->isSubmitted() && $sortieForm->isValid()) {
             $entityManager->persist($sortie);
             $entityManager->flush();
             return $this->redirectToRoute('sortie_liste');
@@ -55,51 +55,51 @@ class SortieController extends AbstractController
 
         return $this->render('sortie/creation.html.twig',
             [
-                "sortieForm"=>$sortieForm->createView()
+                "sortieForm" => $sortieForm->createView()
             ]
         );
     }
 
     #[Route('/detail/{sortie}', name: '_detail')]
-    public function detail (SortieRepository $sortieRepository, Sortie $sortie): Response
+    public function detail(SortieRepository $sortieRepository, Sortie $sortie): Response
     {
         return $this->render('sortie/detail.html.twig', compact('sortie'));
     }
 
 
-
     #[Route('/modification/suppression/{id}', name: '_suppression')]
-    public function suppression (
+    public function suppression(
         EntityManagerInterface $entityManager,
-        SortieRepository $sortieRepository,
-        Request $requete,
-        int $id
+        SortieRepository       $sortieRepository,
+        Request                $requete,
+        int                    $id
     ): Response
     {
-        $sortie=$sortieRepository->find($id);
+        $sortie = $sortieRepository->find($id);
         $sortieForm = $this->createForm(SuppressionType::class, $sortie);
         $sortieForm->handleRequest($requete);
 
-        if ($sortieForm->isSubmitted() ){
+        if ($sortieForm->isSubmitted()) {
 
             $clicked = $requete->request->get('clicked');
-           if ($clicked ==='supprimer') {
+            if ($clicked === 'supprimer') {
 
                 $entityManager->remove($sortie);
                 $entityManager->flush();
             }
-            if ($clicked ==='modifier') {
+            if ($clicked === 'modifier') {
 
                 $entityManager->persist($sortie);
                 $entityManager->flush();
             }
-               return $this->redirectToRoute('sortie_liste');
+            return $this->redirectToRoute('sortie_liste');
         }
-        return $this->render('sortie/suppression.html.twig', compact('sortieForm','sortie'));
+        return $this->render('sortie/suppression.html.twig', compact('sortieForm', 'sortie'));
+    }
 
-    #[Route('/inscription/{sortie}', name: '_inscription')]
-    public function inscription (
-        Sortie $sortie,
+        #[Route('/inscription/{sortie}', name: '_inscription')]
+    public function inscription(
+        Sortie                 $sortie,
         EntityManagerInterface $entityManager
     ): Response
     {
@@ -108,12 +108,13 @@ class SortieController extends AbstractController
         $entityManager->persist($sortie);
         $entityManager->flush();
 
-            return $this->redirectToRoute('sortie_liste');
+        return $this->redirectToRoute('sortie_liste');
     }
 
+
     #[Route('/desistement/{sortie}', name: '_desistement')]
-    public function desistement (
-        Sortie $sortie,
+    public function desistement(
+        Sortie                 $sortie,
         EntityManagerInterface $entityManager
     ): Response
     {
