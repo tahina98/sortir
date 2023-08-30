7e0c8bf0aba74e636173cda9029e9741408bf995
cmathieu2023 <catherine.k.mathieu@gmail.com>
2023-08-30 09:43:08 +0200

    Correction après merge formulaire creation sortie et twig creation sortie

--- src/Form/SortieType.php
+++ src/Form/SortieType.php
@@ -10,6 +10,7 @@ use App\Entity\Sortie;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\SubmitButton;
