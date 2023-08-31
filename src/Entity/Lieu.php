a4335945d905e497a5a2ebb90a98962cfde8252b
cmathieu2023 <catherine.k.mathieu@gmail.com>
2023-08-31 16:45:33 +0200

    Changement regex lieu sortie -

--- src/Entity/Lieu.php
+++ src/Entity/Lieu.php
@@ -18,12 +18,12 @@ class Lieu
 
     #[ORM\Column(length: 255)]
     #[Assert\NotBlank(message: 'Le champ nom de lieu ne doit pas être vide')]
-    #[Assert\Regex('/^[a-zA-Zà-üÀ-Ü0-9\s]{3,35}$/', message: 'le nom ne peut contenir plus de 35 caractères(lettre ou chiffres')]
+    #[Assert\Regex('/^[a-zA-Zà-üÀ-Ü0-9\s\-]{3,50}$/', message: 'le nom ne peut contenir plus de 35 caractères(lettre ou chiffres')]
     private ?string $nom = null;
 
     #[ORM\Column(length: 255)]
     #[Assert\NotBlank(message: 'Le champ nom de lieu ne doit pas être vide')]
-    #[Assert\Regex('/^[a-zA-Zà-üÀ-Ü0-9\s]{3,50}$/', message: 'La rue ne peut contenir que des lettres ou chiffres')]
+    #[Assert\Regex('/^[a-zA-Zà-üÀ-Ü0-9\s\-]{3,50}$/', message: 'La rue ne peut contenir que des lettres ou chiffres')]
     private ?string $rue = null;
 
     #[ORM\Column(nullable: true)]
